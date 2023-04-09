9a1457cb6d93d1754d576e356b5d09be7d4e48b2
vinothkumarFSD <125821734+vinothkumarFSD@users.noreply.github.com>
2023-04-09 12:35:11 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,10 @@
+function user() {
+    var firstname = document.getElementById("firstname").value;
+    var middlename = document.getElementById("middlename").value;
+    var lastname = document.getElementById("lastname").value;
+    var email = document.getElementById("email").value;
+    console.log("firstname :", firstname);
+    console.log("middlename :", middlename);
+    console.log("lastname :", lastname);
+    console.log("email :", email);
+}
